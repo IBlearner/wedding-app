910fa86cb2b4586e09ce59ac6dee72000217f8ce
kienvi <kienvi96@hotmail.com>
2026-02-01 16:33:48 +1000

    Home page hero image tweak.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,34 @@
+"use client";
 import Link from "next/link";
-import myImage from "../assets/images/photo1.jpg";
+import img1 from "../assets/images/photo1.jpg";
 import styles from "./page.module.css";
 import "./styles.scss";
 import Image from "next/image";
+import { useState, useEffect } from "react";
+
+const images = [img1, img1, img1];
 
 export default function Home() {
+	const [activeImg, setActiveImg] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setActiveImg((prev) => (prev + 1) % images.length);
+		}, 2000);
+
+		return () => clearInterval(interval);
+	}, []);
+
 	return (
 		<>
 			<div className="centre_container">
-				<Image className="home_image" src={myImage} width={300} height={400} alt="home_picture" />
+				<Image
+					className={`home_image ${true ? "visible" : ""}`}
+					src={images[activeImg]}
+					width={300}
+					height={400}
+					alt="home_picture"
+				/>
 				<h1 className="name_heading">
 					Kien Vi
 					<br /> <span className="centered-and">and</span>
