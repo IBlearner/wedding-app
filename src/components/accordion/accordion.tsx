11d5a222619238a18a6c92a8eb240ea1ed5bd692
kienvi <kienvi96@hotmail.com>
2026-02-05 19:09:02 +1000

    Fix FAQ.

--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -2,10 +2,20 @@
 import { useState } from "react";
 import { ChevronDown } from "lucide-react";
 import "./styles.scss";
+import { ReactNode } from "react";
+import { Klee_One, Courgette } from "next/font/google";
+
+const KleeOne = Klee_One({
+	weight: ["400"],
+});
+
+const Courgettes = Courgette({
+	weight: ["400"],
+});
 
 interface AccordionProps {
 	question: string;
-	answer: string;
+	answer: ReactNode;
 }
 
 export default function Accordion({ question, answer }: AccordionProps) {
@@ -17,11 +27,11 @@ export default function Accordion({ question, answer }: AccordionProps) {
 
 	return (
 		<div className={`accordion ${isOpen ? "open" : "close"}`} onClick={() => toggleAccordion()}>
-			<button className="accordion-question">
+			<button className={`accordion-question ${Courgettes.className}`}>
 				<span>{question}</span>
 				<ChevronDown />
 			</button>
-			<div className="accordion-answer">{answer}</div>
+			<div className={`accordion-answer ${KleeOne.className}`}>{answer}</div>
 		</div>
 	);
 }
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -7,74 +7,69 @@ import { useWeddingDateCountdown } from "@/hooks/countdown";
 import { Bodoni_Moda, Hachi_Maru_Pop } from "next/font/google";
 
 const HachiPop = Hachi_Maru_Pop({
-    weight: ["400"],
+	weight: ["400"],
 });
 
 const Bodoni = Bodoni_Moda({
-    weight: ["400"],
+	weight: ["400"],
 });
 
 export default function Menu() {
-    const [isOpen, setIsOpen] = useState(false);
-    const router = useRouter();
+	const [isOpen, setIsOpen] = useState(false);
+	const router = useRouter();
 
-    const navigateTo = (target: string) => {
-        router.push(target);
-        setIsOpen(false);
-    };
+	const navigateTo = (target: string) => {
+		router.push(target);
+		setIsOpen(false);
+	};
 
-    const timeLeft = useWeddingDateCountdown();
+	const timeLeft = useWeddingDateCountdown();
 
-    return (
-        <>
-            {/* <button className="menu-home" onClick={() => navigateTo("/")}>
-				<House size={32} />
-			</button>
-			<button className="menu-button" onClick={() => setIsOpen(true)}>
-				<MenuIcon size={32} />
-			</button> */}
-            <div className="menu-mobile-bar">
-                <button className="menu-mobile-button" onClick={() => navigateTo("/")}>
-                    <House size={32} />
-                </button>
-                <div className="menu-mobile-countdown">
-                    <div className={`menu-mobile-countdown-time ${HachiPop.className}`}>
-                        {timeLeft?.days} • {timeLeft?.hours} • {timeLeft?.minutes} • {timeLeft?.seconds}
-                    </div>
-                    <div className={`menu-mobile-countdown-text ${HachiPop.className}`}>days to go</div>
-                </div>
-                <button className="menu-mobile-button" onClick={() => setIsOpen(true)}>
-                    <MenuIcon size={32} />
-                </button>
-            </div>
-            <div className={`menu-overlay ${isOpen ? "open" : "close"}`} onClick={() => setIsOpen(false)}></div>
-            <div className={`menu ${Bodoni.className} ${isOpen ? "open" : "close"}`}>
-                <button className="menu-close" onClick={() => setIsOpen(false)}>
-                    <X />
-                </button>
-                <ul>
-                    <li onClick={() => navigateTo("/")}>
-                        <House />
-                        HOME
-                    </li>
-                    <li onClick={() => navigateTo("/rsvp")}>
-                        <MailQuestionMark />
-                        RSVP
-                    </li>
-                    <li onClick={() => navigateTo("/faq")}>
-                        <MessageCircleQuestionMark />
-                        FAQ
-                    </li>
-                    <li onClick={() => navigateTo("/schedule")}>
-                        <CalendarDays />
-                        SCHEDULE
-                    </li>
-                    <li onClick={() => navigateTo("/travelAccomodation")}>
-                        <Hotel />
-                        LOCATION
-                    </li>
-                </ul>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="menu-mobile-bar">fdsnifsjdn</div>
+			<div className="menu-mobile-bar">
+				<button className="menu-mobile-button" onClick={() => navigateTo("/")}>
+					<House size={32} />
+				</button>
+				<div className="menu-mobile-countdown">
+					<div className={`menu-mobile-countdown-time ${HachiPop.className}`}>
+						{timeLeft?.days} • {timeLeft?.hours} • {timeLeft?.minutes} • {timeLeft?.seconds}
+					</div>
+					<div className={`menu-mobile-countdown-text ${HachiPop.className}`}>days to go</div>
+				</div>
+				<button className="menu-mobile-button" onClick={() => setIsOpen(true)}>
+					<MenuIcon size={32} />
+				</button>
+			</div>
+			<div className={`menu-overlay ${isOpen ? "open" : "close"}`} onClick={() => setIsOpen(false)}></div>
+			<div className={`menu ${Bodoni.className} ${isOpen ? "open" : "close"}`}>
+				<button className="menu-close" onClick={() => setIsOpen(false)}>
+					<X />
+				</button>
+				<ul>
+					<li onClick={() => navigateTo("/")}>
+						<House />
+						HOME
+					</li>
+					<li onClick={() => navigateTo("/rsvp")}>
+						<MailQuestionMark />
+						RSVP
+					</li>
+					<li onClick={() => navigateTo("/faq")}>
+						<MessageCircleQuestionMark />
+						FAQ
+					</li>
+					<li onClick={() => navigateTo("/schedule")}>
+						<CalendarDays />
+						SCHEDULE
+					</li>
+					<li onClick={() => navigateTo("/travelAccomodation")}>
+						<Hotel />
+						LOCATION
+					</li>
+				</ul>
+			</div>
+		</>
+	);
 }
