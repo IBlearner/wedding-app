18fb55a41ac81f421a40a799254a317fb723649e
kienvi <kienvi96@hotmail.com>
2026-02-08 16:05:23 +1000

    Home styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import img1 from "../assets/images/homephoto3.jpg";
 import img2 from "../assets/images/photo2.jpg";
-import img3 from "../assets/images/photo3.jpg";
 import styles from "./page.module.css";
 import "./styles.scss";
 import Image from "next/image";
@@ -12,20 +11,20 @@ export default function Home() {
 		<>
 			<main>
 				<Image className={`home-image`} src={img1} alt="picture of couple" fill />
-				<Image className={`home-image2`} src={img2} alt="picture of couple" fill />
-				<div className="centre_container">
-					<div id="home-page-date">11th April 2026</div>
-					{/* <Link href="/faq">
+				{/* <Image className={`home-image2`} src={img2} alt="picture of couple" fill /> */}
+				{/* <div className="centre_container"> */}
+				<div id="home-page-date">11th April 2026</div>
+				{/* <Link href="/faq">
                         <button id="home-page-rsvp-button" rel="noopener noreferrer">
                             FAQ
                         </button>
                     </Link> */}
-					<Link id="home-page-rsvp" href="/rsvp">
-						<button id="home-page-rsvp-button" rel="noopener noreferrer">
-							RSVP
-						</button>
-					</Link>
-				</div>
+				<Link id="home-page-rsvp" href="/rsvp">
+					<button id="home-page-rsvp-button" rel="noopener noreferrer">
+						RSVP
+					</button>
+				</Link>
+				{/* </div> */}
 			</main>
 			<footer className={styles.footer}></footer>
 		</>
